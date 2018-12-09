70c815d6f896f39c7c92f06fe7de7c7f72615d10
Christopher Rzechula <christopherrzechula@gmail.com>
2018-12-09 15:06:28 -0800

    Able to read characters or lines

--- files/file_readChar.s
+++ files/file_readChar.s
@@ -10,26 +10,36 @@
 @ Post-condition  @
 @ R0: --          @
 @ R1: Character   @
-@ R2: Successful  @
+@ R2: End of file @
 @@@@@@@@@@@@@@@@@@@
 .data
-char: .space 2
+char: .space 1
 
 .text
 .global file_readChar
 file_readChar:
-	push	{R7}
+	push	{R0,R7}
 
 	ldr	R1,=char	@place to store char
 	mov	R2,#1		@how many bytes to read
 	mov	R7,#3		@code to read file
 	svc	0
 
-	mov	R2,R0		@R2 = bytes read (1 or 0)
+	cmp	R0,#0
+	beq	end_of_file
+
+	mov	R2,#0		@if(bytesRead != 0) not end of file
 
 	ldrb	R1,[R1]		@load character from address
-	
-	pop	{R7}
+	b	return
+
+end_of_file:
+	mov	R1,#0		@make the character returned null
+	mov	R2,#1		@return true for end of file
+	b	return
+
+return:
+	pop	{R0,R7}
 	bx	LR
 .end
 
--- files/file_readLine.s
+++ files/file_readLine.s
@@ -8,10 +8,12 @@
 @ Pre-condition   @
 @ R0: File handle @
 @ R1: --          @
+@ R2: --          @
 @=================@
 @ Post-condition  @
 @ R0: --          @
 @ R1: String      @
+@ R2: End of file @
 @@@@@@@@@@@@@@@@@@@
 .data
 strBuf: .space 256
@@ -23,43 +25,61 @@ file_readLine:
 @ Register Alias @
 file     .req R0 @
 char     .req R1 @
-strBuf   .req R2 @
-bufCount .req R3 @
+strBuf   .req R3 @
+bufCount .req R4 @
+null     .req R5 @
 @@@@@@@@@@@@@@@@@@
-	push	{R1-R3,LR}
+	push	{R0,R3-R5,LR}
 
 	ldr	strBuf,=strBuf
+	mov	null,#0			@contant null
 
 loop:
 	push	{strBuf}		@save strBuf
 
 	bl	file_readChar
 
-	cmp	R2,#0
-	beq	return			@if(!readChar) end of file reached
+	cmp	R2,#1
+	beq	end_of_file		@if(readChar.eof()) end of file reached
 
 	pop	{strBuf}		@load strBuf
 
 	cmp	char,#'\r'
-	beq	return			@if(char == \r) line is finished
+	beq	carriage_found		@if(char == \r) line is finished
 
 	cmp	char,#'\n'
-	beq	return			@if(char == \n) line is finished
+	beq	end_line_found		@if(char == \n) line is finished
 
 	cmp	bufCount,#255
-	beq	return			@if(bufCount == 255) force finish
+	beq	end_line_found		@if(bufCount == 255) force finish
 
 	strb	char,[strBuf],#1	@store char in buffer and offset ptr
 	add	bufCount,#1
 
 	b	loop
 
-return:
-	mov	R1,strBuf
-	bl	string_copy
-	mov	R1,R0
 
-	pop	{R1-R3,LR}
+carriage_found:
+	bl	file_readChar		@\r is always followed by a \n
+	b	end_line_found
+
+end_line_found:
+	strb	null,[strBuf]		@store a null terminator to end string
+	b	make_string
+
+make_string:
+	ldr	strBuf,=strBuf
+	mov	R1,strBuf
+	bl	string_copy		@create deep copy of string
+	mov	R1,R0			@return it in R1
+	b	return
+
+end_of_file:
+	pop	{strBuf}
+	b	return
+
+return:
+	pop	{R0,R3-R5,LR}
 	bx	LR
 .end
 
